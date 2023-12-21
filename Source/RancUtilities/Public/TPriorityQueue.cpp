78e073d251f712548e454e3dadfc60587ef5eddc
Jacob <jjonj@hotmail.com>
2023-12-22 00:19:17 +0100

    Added Integer priority Queue (using cost)

---
+++ Source/RancUtilities/Public/TPriorityQueue.cpp
@@ -0,0 +1,87 @@
+﻿#include "TPriorityQueue.h"
+
+FPriorityQueueNode::FPriorityQueueNode() : DataInteger(0), Cost(0.0f) {}
+
+FPriorityQueueNode::FPriorityQueueNode(int InDataInteger, float InPriority)
+	: DataInteger(InDataInteger), Cost(InPriority) {}
+
+bool FPriorityQueueNode::operator<(const FPriorityQueueNode& Other) const
+{
+	return Cost < Other.Cost;
+}
+
+UPriorityQueue::UPriorityQueue()
+{
+	Array.Heapify();
+}
+
+void UPriorityQueue::SetInitialCapacity(int InitialCapacity)
+{
+	ContainsSet.Reserve(InitialCapacity);
+	Array.Reserve(InitialCapacity);
+}
+
+void UPriorityQueue::Clear()
+{
+	Array.Empty();
+	ContainsSet.Empty();
+}
+
+int UPriorityQueue::Pop(UPARAM(ref) bool& Success)
+{
+	if (ContainsSet.IsEmpty())
+	{
+		Success = false;
+		return -80085;
+	}
+	
+	FPriorityQueueNode Node;
+	Array.HeapPop(Node);
+	ContainsSet.Remove(Node.DataInteger);
+
+	Success = true;
+	return Node.DataInteger;
+}
+
+void UPriorityQueue::Push(int DataInteger, float Cost)
+{
+	ContainsSet.Add(DataInteger);
+	Array.HeapPush(FPriorityQueueNode(DataInteger, Cost));
+}
+
+void UPriorityQueue::Remove(int DataInteger)
+{
+	for (int i = 0; i < Array.Num(); i++)
+	{
+		if (Array[i].DataInteger == DataInteger)
+		{
+			Array.HeapRemoveAt(i);
+			ContainsSet.Remove(DataInteger);
+			break;
+		}
+	}
+}
+
+void UPriorityQueue::Replace(int DataInteger, float NewCost)
+{
+	for (int i = 0; i < Array.Num(); i++)
+	{
+		if (Array[i].DataInteger == DataInteger)
+		{
+			Array[i].Cost = NewCost;
+			break;
+		}
+	}
+
+	Array.HeapSort();
+}
+
+bool UPriorityQueue::Contains(int DataInteger)
+{
+	return ContainsSet.Contains(DataInteger);
+}
+
+bool UPriorityQueue::IsEmpty() const
+{
+	return Array.Num() == 0;
+}
---
+++ Source/RancUtilities/Public/TPriorityQueue.h
@@ -0,0 +1,57 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "TPriorityQueue.generated.h"
+
+USTRUCT(BlueprintType)
+struct FPriorityQueueNode
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PriorityQueue")
+	int DataInteger;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PriorityQueue")
+	float Cost;
+
+	FPriorityQueueNode();
+	FPriorityQueueNode(int DataInteger, float Priority);
+	bool operator<(const FPriorityQueueNode& Other) const;
+};
+
+UCLASS(Blueprintable, EditInlineNew)
+class UPriorityQueue : public UObject
+{
+	GENERATED_BODY()
+    
+public:
+	UPriorityQueue();
+
+	UFUNCTION(BlueprintCallable, Category = "PriorityQueue")
+	void SetInitialCapacity(int InitialCapacity);
+	
+	UFUNCTION(BlueprintCallable, Category = "PriorityQueue")
+	void Clear();
+
+	UFUNCTION(BlueprintCallable, Category = "PriorityQueue")
+	int Pop(bool& Success);
+
+	UFUNCTION(BlueprintCallable, Category = "PriorityQueue")
+	void Push(int DataInteger, float Cost);
+	
+	UFUNCTION(BlueprintCallable, Category = "PriorityQueue")
+	void Remove(int DataInteger);
+
+	UFUNCTION(BlueprintCallable, Category = "PriorityQueue")
+	void Replace(int DataInteger, float NewCost);
+	
+	UFUNCTION(BlueprintCallable, Category = "PriorityQueue")
+	bool Contains(int DataInteger);
+
+	UFUNCTION(BlueprintCallable, Category = "PriorityQueue")
+	bool IsEmpty() const;
+
+private:
+	TArray<FPriorityQueueNode> Array;
+	TSet<int> ContainsSet;
+};
