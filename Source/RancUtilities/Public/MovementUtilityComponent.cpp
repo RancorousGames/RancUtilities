34edfcc6ffbdd35e0ed4bfa8e7d63db9f50daba8
Jacob <jjonj@hotmail.com>
2024-01-25 23:32:15 +0100

    Added new MovementUtilityComponent, currently only supports rotating to a target yaw, used with e.g. melee attacks

---
+++ Source/RancUtilities/Public/MovementUtilityComponent.cpp
@@ -0,0 +1,60 @@
+﻿#include "MovementUtilityComponent.h"
+
+#include "MovementUtilityComponent.h"
+
+UMovementUtilityComponent::UMovementUtilityComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UMovementUtilityComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UMovementUtilityComponent::SmoothRotateToYaw(float NewTargetYaw, float NewRotationSpeed)
+{
+	TargetYaw = NewTargetYaw;
+	RotationSpeed = NewRotationSpeed;
+
+	// Start rotation immediately on the next tick
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle_RotationUpdate, this, &UMovementUtilityComponent::UpdateRotation, GetWorld()->GetDeltaSeconds(), true);
+}
+
+void UMovementUtilityComponent::UpdateRotation()
+{
+	AActor* Owner = GetOwner();
+	if (!Owner)
+	{
+		FinishRotation();
+		return;
+	}
+
+	// Get the delta time from the world
+	float DeltaTime = GetWorld()->GetDeltaSeconds();
+
+	// Get current rotation as a quaternion
+	FQuat CurrentQuat = Owner->GetActorRotation().Quaternion();
+	// Create target rotation as a quaternion
+	FQuat TargetQuat = FQuat(FRotator(0.f, TargetYaw, 0.f));
+
+	// Perform spherical interpolation between the current and target rotations
+	FQuat NewQuat = FQuat::Slerp(CurrentQuat, TargetQuat, RotationSpeed * DeltaTime);
+
+	// Ensure the quaternion is normalized (SLERP can sometimes return a non-normalized quaternion)
+	NewQuat.Normalize();
+
+	// Set the new rotation based on the interpolated quaternion
+	Owner->SetActorRotation(NewQuat);
+
+	// Check if the target yaw has been reached within a small error margin
+	if (NewQuat.Equals(TargetQuat, 0.01f))
+	{
+		FinishRotation();
+	}
+}
+
+void UMovementUtilityComponent::FinishRotation()
+{
+	GetWorld()->GetTimerManager().ClearTimer(TimerHandle_RotationUpdate);
+}
---
+++ Source/RancUtilities/Public/MovementUtilityComponent.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "MovementUtilityComponent.generated.h"
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class RANCUTILITIES_API UMovementUtilityComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:    
+	// Sets default values for this component's properties
+	UMovementUtilityComponent();
+
+	// Function to start a smooth rotation to a target yaw
+	UFUNCTION(BlueprintCallable, Category="Movement|Rotation")
+	void SmoothRotateToYaw(float TargetYaw, float RotationSpeed = 90.f);
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+private:
+	// Update rotation every frame
+	void UpdateRotation();
+
+	// Complete the rotation
+	void FinishRotation();
+
+	// Target yaw rotation
+	float TargetYaw;
+
+	// Rotation speed in degrees per second
+	float RotationSpeed;
+
+	// Timer handle for rotation update
+	FTimerHandle TimerHandle_RotationUpdate;
+};
