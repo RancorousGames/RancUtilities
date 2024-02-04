f85ee53af6d98b54b0c0afd4f1bb84f6f5abb41b
Jacob <jjonj@hotmail.com>
2024-02-04 15:02:03 +0100

    Added new enum library

---
+++ Source/RancUtilities/Public/RancEnumLibrary.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+UENUM(BlueprintType)
+enum class EGroundType : uint8
+{
+	Ground UMETA(DisplayName = "Ground"),
+	Water UMETA(DisplayName = "Water")
+};
+
+
+UENUM(BlueprintType)
+enum class ERelationship : uint8
+{
+	Friendly,
+	Same,
+	Hostile,
+	Neutral
+};
